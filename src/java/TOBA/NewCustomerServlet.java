6a905714d9caf29836724fa7164412c2723c293c
jhronek <johnhronek@gmail.com>
2016-03-27 18:15:58 -0400

    Assignment 1 committed

---
+++ src/java/TOBA/NewCustomerServlet.java
@@ -0,0 +1,102 @@
+package TOBA;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jhronek
+ */
+public class NewCustomerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String phone = request.getParameter("phone");
+        String address = request.getParameter("address");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String zipcode = request.getParameter("zipcode");
+        String email = request.getParameter("email");
+        User user = new User(firstName, lastName, phone, address, city, state, zipcode, email);
+        
+        request.setAttribute("user", user);
+        String url = "/New_customer.jsp";
+        try {
+            if (firstName == null || lastName == null || phone == null ||
+                    address == null || city == null || state == null ||
+                    zipcode == null || email == null || firstName.isEmpty() ||
+                    lastName.isEmpty() || phone.isEmpty() || address.isEmpty() ||
+                    city.isEmpty() || state.isEmpty() || zipcode.isEmpty() ||
+                    email.isEmpty()) {
+                
+                url = "/New_customer.jsp";
+                String alertMessage = "<div class=\"alert alert-danger\">\n" +
+"                            <strong>Alert!</strong>&nbsp;Please fill out all fields!\n</div>";
+                request.setAttribute("alertMessage", alertMessage);
+            }
+            else {
+                url = "/Success.jsp";
+            }
+            
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/Error_404.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : Error_404
+    Created on : Mar 26, 2016, 4:53:36 PM
+    Author     : jhronek
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>404 Error</title>
+    </head>
+    <body>
+        <h1>404 Error</h1>
+        <p>Sorry, the server was not able to find the file you requested.</p>
+        <p>Click the Back button to continue.</p>
+    </body>
+</html>
